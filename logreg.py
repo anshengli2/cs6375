0fd62293a3a89d59192f93fe3e6ec17f58479156
Ansheng Li <34924044+anshengli2@users.noreply.github.com>
2022-12-01 01:01:13 -0600

    new new

--- logreg.py
+++ logreg.py
@@ -10,14 +10,20 @@ We want to find the best values for our weights
 
 
 class LogisticRegression():
-    def __init__(self, fit_intercept=True, verbose=False):
+    def __init__(self, fit_intercept=True, verbose=True):
         self.fit_intercept = fit_intercept
         self.verbose = verbose
+        self.loss_history = []
+        self.iter_history = []
 
     def add_intercept(self, X):
         intercept = np.ones((X.shape[0], 1))
         return np.concatenate((intercept, X), axis=1)
 
+    # Cost function finds the error between actual value and predicted value
+    # Hypothesis is nonconvex function, meaning there are chances of
+    # finding the local minima and avoiding the global minima
+    # Smoothened the curve with the help of log and our cost function will look like this
     def loss(self, h, y):
         return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
 
@@ -28,7 +34,13 @@ class LogisticRegression():
     sigmoid(z) = 1 / (1 + e^-(z))
     """
 
-    def fit_sigmoid(self, X, y, learning_rate=0.01, num_iter=10000):
+    """
+    Gradient descent - derivative of the loss function with respect to the weights
+    The goal is to minimize the loss (cost function) by increasing or decreasing the weights (fitting)
+    The weights are updated by subtracting the gradient times the learning rate
+    """
+
+    def fit_sigmoid(self, X, y, learning_rate=0.01, num_iter=1000):
         if self.fit_intercept:
             X = self.add_intercept(X)
 
@@ -41,41 +53,12 @@ class LogisticRegression():
             gradient = np.dot(X.T, (h - y)) / y.shape[0]
             self.weights -= learning_rate * gradient
 
-            z = np.dot(X, self.weights)
-            h = 1 / (1 + np.exp(-z))
-            loss = self.loss(h, y)
-
-            if(self.verbose == True and i % 10000 == 0):
-                print(f'loss: {loss} \t')
-
-    """
-    Gradient descent - derivative of the loss function with respect to the weights
-    The goal is to minimize the loss by increasing or decreasing the weights (fitting)
-    The weights are updated by subtracting the gradient times the learning rate
-    """
-
-    def fit_mle(self, X, y, learning_rate=0.01, num_iter=10000):
-        if self.fit_intercept:
-            X = self.add_intercept(X)
-
-        # weights initialization
-        self.weights = np.zeros(X.shape[1])
-
-        for i in range(num_iter):
-
-            z = np.dot(X, self.weights)
-            h = 1 / (1 + np.exp(-z))
-            ll = np.sum(y*z - np.log(1 + np.exp(z)))
-            gradient = np.dot(X.T, (y-h))
-            self.weights += learning_rate * gradient
-
-            z = np.dot(X, self.weights)
-            h = 1 / (1 + np.exp(-z))
-            ll = np.sum(y*z - np.log(1 + np.exp(z)))
-            loss = self.loss(ll, y)
-
-            if(self.verbose == True and i % 10000 == 0):
-                print(f'loss: {loss} \t')
+            if(self.verbose == True and i % 500 == 0):
+                z = np.dot(X, self.weights)
+                h = 1 / (1 + np.exp(-z))
+                loss = self.loss(h, y)
+                self.loss_history.append(loss)
+                self.iter_history.append(i)
 
     def predict_prob(self, X):
         if self.fit_intercept:
--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 
 import matplotlib.pyplot as plt
-from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
-from sklearn.datasets import make_classification
 from sklearn import metrics
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 import warnings
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from logreg import LogisticRegression as CustomLogisticRegression
+from logreg2 import LogitRegression as NewLogReg
+import seaborn as sn
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.model_selection import GridSearchCV
 
 
-def confusion_matrix(ytst, y_pred):
+def confusionMatrix(ytst, y_pred):
     fp = 0
     fn = 0
     tp = 0
@@ -47,29 +51,90 @@ data['Label'] = data['Label'].apply(lambda x: 1 if x == "UP" else 0)
 data['Sector'].replace(['Consumer Discretionary', 'Health Care', 'Finance', 'Technology',
                         'Industrials', 'Miscellaneous', 'Utilities', 'Telecommunications',
                         'Real Estate', 'Energy', 'Consumer Staples', 'Basic Materials'],
-                       [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], inplace=True)
+                       [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], inplace=True)
 
 X = data.drop(['Symbol', 'Start', 'End', 'Label'], axis=1).copy()
 y = data['Label'].copy()
 
+# print(pd.DataFrame(X).describe().apply(lambda s: s.apply('{0:.5f}'.format)))
 x_train, x_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+# print(pd.DataFrame(x_train).describe().apply(
+#     lambda s: s.apply('{0:.5f}'.format)))
 
-warnings.filterwarnings('ignore')
 lr = CustomLogisticRegression()
 lr.fit_sigmoid(x_train, y_train, learning_rate=0.1, num_iter=10000)
-# lr.fit_mle(x_train, y_train)
 y_pred = lr.predict(x_test)
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy Score:", accuracy)
-confusion_matrix(y_test, y_pred)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+df = pd.DataFrame({'x': [x for x in range(len(x_test))],
+                   'y': [y_l for y_l in y_test],
+                   'y_pred_prob': np.array(lr.predict_prob(x_test))})
+df['y'] = df['y'].apply(lambda x: "UP" if x == 1 else "DOWN")
+sn.scatterplot(data=df, x='x', y='y_pred_prob', style='y', hue='y')
+print(df)
+plt.legend(loc='upper right')
+ax.set_title("Decision Boundary")
+ax.set_xlabel('N/2')
+ax.set_ylabel('Predicted Probability')
+plt.axhline(.5, color='black')
+plt.show()
+
+warnings.filterwarnings('ignore')
+learningRate = [0.001, 0.01, 0.1]
+models = []
+# for index, i in enumerate(learningRate):
+#     lr = CustomLogisticRegression()
+#     lr.fit_sigmoid(x_train, y_train, learning_rate=i, num_iter=10000)
+#     print("Final weights", lr.weights)
+#     print("Final cost", lr.loss_history[-1])
+#     models.append([lr.loss_history, lr.iter_history])
+#     y_pred = lr.predict(x_test)
+#     accuracy = accuracy_score(y_test, y_pred)
+#     print("Accuracy Score:", accuracy)
+
+#     # plt.figure(figsize=(10, 6))
+#     # plt.scatter(X[y == 0][:, 0], X[y == 0][:, 1], color='b', label='0')
+#     # plt.scatter(X[y == 1][:, 0], X[y == 1][:, 1], color='r', label='1')
+#     # plt.legend()
+#     # x1_min, x1_max = X[:, 0].min(), X[:, 0].max(),
+#     # x2_min, x2_max = X[:, 1].min(), X[:, 1].max(),
+#     # xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max),
+#     #                        np.linspace(x2_min, x2_max))
+#     # grid = np.c_[xx1.ravel(), xx2.ravel()]
+#     # probs = lr.predict_prob(grid).reshape(xx1.shape)
+#     # plt.contour(xx1, xx2, probs, [0.5], linewidths=1, colors='black')
+#     # Confusion Matrix
+#     ax = plt.axes()
+#     cf_matrix = metrics.confusion_matrix(y_test, y_pred)
+#     print(cf_matrix)
+#     s = sn.heatmap(cf_matrix, annot=True, fmt='g', ax=ax)
+#     s.set(xlabel='Predicted Label', ylabel='True Label')
+#     ax.set_title("Learning rate = " +
+#                  str(learningRate[index]) + "     Accuracy = " + str(accuracy))
+#     plt.show()
+
+# for i, model in enumerate(models):
+#     plt.plot(model[1], model[0], label="Learning rate = " +
+#              str(learningRate[i]))
+
+
+# plt.xlabel("Iterations")
+# plt.ylabel("Cost")
+# plt.legend()
+# plt.show()
 
 # print("\nSklearn's logistic regression")
-# logisticRegr = LogisticRegression()
+# logisticRegr = LogisticRegression(C=1)
 # logisticRegr.fit(x_train, y_train)  # apply scaling on training data
 # # make predictions on the test set
 # y_pred = logisticRegr.predict(x_test)
 # accuracy = accuracy_score(y_test, y_pred)
 # print("Accuracy Score:", accuracy)
 # # measure accuracy
-# confusion_matrix(y_pred, y_pred)
+# plot_confusion_matrix(logisticRegr, x_test, y_test)
+# plt.show()
