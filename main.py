c32b9e79f4700a6e31304900815f5bf83bb3aad9
Ansheng Li <34924044+anshengli2@users.noreply.github.com>
2022-12-02 16:26:47 -0600

    new

--- main.py
+++ main.py
@@ -1,46 +1,19 @@
 
+from sklearn.model_selection import KFold
+from random import randrange
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
 import warnings
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from logreg import LogisticRegression as CustomLogisticRegression
-from logreg2 import LogitRegression as NewLogReg
 import seaborn as sn
 from sklearn.metrics import plot_confusion_matrix
-from sklearn.model_selection import GridSearchCV
-
-
-def confusionMatrix(ytst, y_pred):
-    fp = 0
-    fn = 0
-    tp = 0
-    tn = 0
-    for actual_value, predicted_value in zip(ytst, y_pred):
-        # see if it's a true or false prediction
-        if predicted_value == actual_value:  # t
-            if predicted_value == 1:  # tp
-                tp += 1
-            else:  # tn
-                tn += 1
-        else:  # f
-            if predicted_value == 1:  # fp
-                fp += 1
-            else:  # fn
-                fn += 1
-
-    our_confusion_matrix = [
-        [tp, fn],
-        [fp, tn]
-    ]
-    our_confusion_matrix = np.array(our_confusion_matrix)
-    print(our_confusion_matrix)
-
+from sklearn.model_selection import cross_val_score
 
 data = pd.read_csv("training_set.csv", index_col=[0])
 data = data.copy()
@@ -56,13 +29,17 @@ data['Sector'].replace(['Consumer Discretionary', 'Health Care', 'Finance', 'Tec
 X = data.drop(['Symbol', 'Start', 'End', 'Label'], axis=1).copy()
 y = data['Label'].copy()
 
-x_train, x_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42)
+# Split a dataset into a train and test set
+
+
+# x_train, x_test, y_train, y_test = train_test_split(
+#     X, y, test_size=0.2, random_state=42)
 
 # Standardizing the feature values
-sc_x = StandardScaler()
-x_train = sc_x.fit_transform(x_train)
-x_test = sc_x.transform(x_test)
+# sc_x = StandardScaler()
+# X = sc_x.fit_transform(X)
+# print(X)
+# x_test = sc_x.transform(x_test)
 
 """
 Logistic Regression
@@ -70,39 +47,65 @@ Logistic Regression
 warnings.filterwarnings('ignore')
 learningRate = [0.001, 0.01, 0.1]
 models = []
-for index, i in enumerate(learningRate):
-    lr = CustomLogisticRegression()
-    lr.fit_sigmoid(x_train, y_train, learning_rate=i, num_iter=10000)
-    print("Final weights", lr.weights)
-    print("Final cost", lr.loss_history[-1])
-    models.append([lr.loss_history, lr.iter_history])
-    y_pred = lr.predict(x_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    print("Accuracy Score:", accuracy)
 
-    # Decision boundary
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    df = pd.DataFrame({'x': [x for x in range(len(x_test))],
-                       'y': [y_l for y_l in y_test],
-                       'y_pred_prob': np.array(lr.predict_prob(x_test))})
-    df['y'] = df['y'].apply(lambda x: "UP" if x == 1 else "DOWN")
-    sn.scatterplot(data=df, x='x', y='y_pred_prob', style='y', hue='y')
-    plt.legend(loc='upper right')
-    ax.set_title("Decision Boundary   Accuracy=" + str(accuracy))
-    ax.set_xlabel('N/2')
-    ax.set_ylabel('Predicted Probability')
-    plt.axhline(.5, color='black')
-    plt.show()
+kf = KFold(n_splits=10)
+for i in learningRate:
+    k = 1
+    acc = []
+    for train_index, test_index in kf.split(X):
+        # print("TRAIN:", train_index, "TEST:", test_index)
+        x_train, x_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+        sc_x = StandardScaler()
+        x_train = sc_x.fit_transform(x_train)
+        x_test = sc_x.transform(x_test)
+        lr = CustomLogisticRegression()
+        lr.fit_sigmoid(x_train, y_train, learning_rate=i, num_iter=10000)
+        # print("Final weights", lr.weights)
+        # print("Final cost", lr.loss_history[-1])
+        y_pred = lr.predict(x_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        # print("Accuracy Score K={}: {}".format(k, accuracy))
+        acc.append(accuracy)
+        k += 1
+    models.append(acc)
+    print("LR: {} Min: {} Max: {} Avg: {}".format(
+        i, np.min(acc), np.max(acc), np.average(acc)))
 
-    # Confusion Matrix
-    ax = plt.axes()
-    cf_matrix = metrics.confusion_matrix(y_test, y_pred)
-    s = sn.heatmap(cf_matrix, annot=True, fmt='g', ax=ax)
-    s.set(xlabel='Predicted Label', ylabel='True Label')
-    ax.set_title("Learning rate = " +
-                 str(learningRate[index]) + "     Accuracy = " + str(accuracy))
-    plt.show()
+
+# for index, i in enumerate(learningRate):
+#     lr = CustomLogisticRegression()
+#     lr.fit_sigmoid(x_train, y_train, learning_rate=i, num_iter=10000)
+#     print("Final weights", lr.weights)
+#     print("Final cost", lr.loss_history[-1])
+#     models.append([lr.loss_history, lr.iter_history])
+#     y_pred = lr.predict(x_test)
+#     accuracy = accuracy_score(y_test, y_pred)
+#     print("Accuracy Score:", accuracy)
+
+#     # Decision boundary
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111)
+#     df = pd.DataFrame({'x': [x for x in range(len(x_test))],
+#                        'y': [y_l for y_l in y_test],
+#                        'y_pred_prob': np.array(lr.predict_prob(x_test))})
+#     df['y'] = df['y'].apply(lambda x: "UP" if x == 1 else "DOWN")
+#     sn.scatterplot(data=df, x='x', y='y_pred_prob', style='y', hue='y')
+#     plt.legend(loc='upper right')
+#     ax.set_title("Decision Boundary   Accuracy=" + str(accuracy))
+#     ax.set_xlabel('N/2')
+#     ax.set_ylabel('Predicted Probability')
+#     plt.axhline(.5, color='black')
+#     plt.show()
+
+#     # Confusion Matrix
+#     ax = plt.axes()
+#     cf_matrix = metrics.confusion_matrix(y_test, y_pred)
+#     s = sn.heatmap(cf_matrix, annot=True, fmt='g', ax=ax)
+#     s.set(xlabel='Predicted Label', ylabel='True Label')
+#     ax.set_title("Learning rate = " +
+#                  str(learningRate[index]) + "     Accuracy = " + str(accuracy))
+#     plt.show()
 
 # for i, model in enumerate(models):
 #     plt.plot(model[1], model[0], label="Learning rate = " +
@@ -120,7 +123,12 @@ for index, i in enumerate(learningRate):
 # # make predictions on the test set
 # y_pred = logisticRegr.predict(x_test)
 # accuracy = accuracy_score(y_test, y_pred)
+# lr = CustomLogisticRegression()
+# lr.fit_sigmoid(x_train, y_train, learning_rate=0.1, num_iter=10000)
+# scores = cross_val_score(lr, x_train, y_train, cv=10)
+# print('Cross-Validation Accuracy Scores', scores)
+# scores = pd.Series(scores)
+# print(scores.min(), scores.mean(), scores.max())
 # print("Accuracy Score:", accuracy)
-# # measure accuracy
 # plot_confusion_matrix(logisticRegr, x_test, y_test)
 # plt.show()
